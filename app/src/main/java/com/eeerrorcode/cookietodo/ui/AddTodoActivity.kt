d762083690e643a1d7af7a7eb1211a69801bef88
jungyoonn <sophia7625@naver.com>
2025-01-22 15:07:13 +0900

    todo item 추가 액티비티

---
+++ app/src/main/java/com/eeerrorcode/cookietodo/ui/AddTodoActivity.kt
@@ -0,0 +1,40 @@
+package com.eeerrorcode.cookietodo.ui
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.eeerrorcode.cookietodo.R
+import com.eeerrorcode.cookietodo.data.Todo
+import com.eeerrorcode.cookietodo.data.TodoDatabase
+import kotlinx.coroutines.launch
+
+class AddTodoActivity: AppCompatActivity() {
+    private lateinit var todoDatabase: TodoDatabase
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_todo_activity)
+
+        todoDatabase = TodoDatabase.getDatabase(this)
+
+        val inputTitle = findViewById<EditText>(R.id.inputTitle)
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+
+        btnAdd.setOnClickListener {
+            val title = inputTitle.text.toString()
+            if(title.isNotBlank()) {
+                lifecycleScope.launch {
+                    val newTodo = Todo(title = title)
+                    todoDatabase.todoDao().insert(newTodo)
+
+                    setResult(RESULT_OK)
+                    finish()
+                }
+            } else {
+                inputTitle.error = "필수 입력입니다"
+            }
+        }
+    }
+}
