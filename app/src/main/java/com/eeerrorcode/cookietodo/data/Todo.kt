261957ef03a1d30a012a566cf93810f51183edb6
jungyoonn <sophia7625@naver.com>
2025-01-22 11:21:02 +0900

    엔티티 프라이머리 키 추가

--- app/src/main/java/com/eeerrorcode/cookietodo/data/Todo.kt
+++ app/src/main/java/com/eeerrorcode/cookietodo/data/Todo.kt
@@ -1,9 +1,11 @@
 package com.eeerrorcode.cookietodo.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class Todo (
+    @PrimaryKey(autoGenerate = true)
     var num:Int = 0,
     var title: String ,
     var completed: Boolean
